4755cddebdd9d31760f77570eb3bdb2bcd62072b
Bartek <bartek9393@poczta.onet.pl>
2017-07-16 19:17:49 +0200

    Added taking data from database

--- backend.php
+++ backend.php
@@ -1,4 +1,10 @@
 <?php
+$mysqli = new mysqli('localhost', 'root', '', 'shop');
+
+if ($mysqli->connect_error) {
+    echo "Error connecting to database";
+    die("Connection failed: " . $mysqli->connect_error);
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($_POST['action'] == 'client') {
@@ -11,22 +17,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     if ($_GET['action'] == 'clients') {
-        echo json_encode([
-            'clients' => [
-                ['name' => 'stefek', 'id' => 1, 'age' => 12, 'purchase_price' => 152],
-                ['name' => 'rafal', 'id' => 2, 'age' => 12, 'purchase_price' => 152],
-                ['name' => 'marcin', 'id' => 3, 'age' => 12, 'purchase_price' => 152]
-            ]
-        ]);
+
+        $result = $mysqli->query("SELECT * FROM clients");
+        $array = array();
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            $array[] = $row;
+        }
+
+        echo json_encode(['clients' => $array]);
     }
 
     if ($_GET['action'] == 'products') {
-        echo json_encode([
-            'products' => [
-                ['name' => 'banan', 'id' => 1, 'type' => 'electronic', 'price' => 152],
-                ['name' => 'banan', 'id' => 2, 'type' => 'electronic', 'price' => 152],
-                ['name' => 'banan', 'id' => 3, 'type' => 'electronic', 'price' => 152]
-            ]
-        ]);
+        $result = $mysqli->query("SELECT * FROM products");
+        $array = array();
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            $array[] = $row;
+        }
+
+        echo json_encode(['products' => $array]);
     }
 }
+
+
+$mysqli->close();
