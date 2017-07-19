173e4e73bd8b22bf4660e289fb0540f833e3861f
Bartek <bartek9393@poczta.onet.pl>
2017-07-19 21:08:51 +0200

    Implemented adding new client

--- backend.php
+++ backend.php
@@ -8,13 +8,41 @@ if ($mysqli->connect_error) {
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($_POST['action'] == 'client') {
-        http_response_code(201);
+        $name = $_POST["name"];
+        $age = $_POST["age"];
+        $purchase_price = $_POST["purchase_price"];
+
+        $name = $mysqli->real_escape_string($name);
+        $age = $mysqli->real_escape_string($age);
+        $purchase_price = $mysqli->real_escape_string($purchase_price);
+
+
+        $sql = "INSERT INTO clients (name, age, purchase_price)
+            VALUES ('$name','$age','$purchase_price')";
+
+        if (isValidClient($name, $age, $purchase_price)) {
+            if (mysqli_query($mysqli, $sql)) {
+                echo 'Added new client';
+            }
+        } else {
+            echo "Error";
+        }
     }
 
     if ($_POST['action'] == 'product') {
         http_response_code(201);
     }
 }
+
+function isValidClient($name, $age, $purchase_price)
+{
+    return preg_match('/^[a-ząęóśłńćżź]{3,20}$/i', $name)
+        && preg_match('/^[0-9]{2}$/', $age)
+        && $age > 5 && $age < 90
+        && preg_match('/^[0-9]+(.[0-9]{2})?$/', $purchase_price)
+        && $purchase_price < 10000;
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     if ($_GET['action'] == 'clients') {
 
