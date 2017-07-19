bca71aa7faf55fc32365d5ff027a8dc716e0d756
Bartek <bartek9393@poczta.onet.pl>
2017-07-19 21:24:46 +0200

    Implemented adding new product

--- backend.php
+++ backend.php
@@ -30,7 +30,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     if ($_POST['action'] == 'product') {
-        http_response_code(201);
+        $name = $_POST["name"];
+        $type = $_POST["type"];
+        $price = $_POST["price"];
+
+        $name = $mysqli->real_escape_string($name);
+        $age = $mysqli->real_escape_string($type);
+        $price = $mysqli->real_escape_string($price);
+
+
+        $sql = "INSERT INTO products (name, type, price)
+            VALUES ('$name','$type','$price')";
+
+        if (isValidProduct($name, $type, $price)) {
+            if (mysqli_query($mysqli, $sql)) {
+                echo 'Added new product';
+            }
+        } else {
+            echo "Error";
+        }
     }
 }
 
@@ -41,6 +59,16 @@ function isValidClient($name, $age, $purchase_price)
         && $age > 5 && $age < 90
         && preg_match('/^[0-9]+(.[0-9]{2})?$/', $purchase_price)
         && $purchase_price < 10000;
+}
+
+
+function isValidProduct($name, $type, $price)
+{
+    return preg_match('/^[a-ząęóśłńćżź]{3,20}$/i', $name)
+        && strlen($name)
+        && preg_match('/^[a-ząęóśłńćżź]{3,25}$/i', $type)
+        && preg_match('/^[0-9]+(.[0-9]{2})?$/', $price)
+        && $price < 10000;
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
