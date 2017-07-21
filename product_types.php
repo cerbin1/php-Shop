8c2afb9b99a8f0606be0876bf133e902725d6967
Bartek <bartek9393@poczta.onet.pl>
2017-07-21 17:12:06 +0200

    Implemented adding new type of product to database

--- product_types.php
+++ product_types.php
@@ -1,9 +1,26 @@
 <?php
 $mysqli = include 'connection.php';
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ($_POST['action'] == 'add') {
+        $name = $_POST['type'];
+
+        $name = $mysqli->real_escape_string($name);
+
+        $sql = "INSERT INTO types (name)
+            VALUES ('$name')";
+
+        if ($mysqli->query($sql)) {
+            echo 'Added new type';
+        } else {
+            echo 'Error';
+        }
+    }
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     if ($_GET['action'] == 'type') {
-        $result = $mysqli->query('SELECT type FROM types');
+        $result = $mysqli->query('SELECT name FROM types');
         $array = array();
 
         while ($row = mysqli_fetch_assoc($result)) {
@@ -13,3 +30,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         echo json_encode(['types' => $array]);
     }
 }
+
+$mysqli->close();
